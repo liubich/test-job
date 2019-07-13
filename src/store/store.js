42470e51ed97014adb08bdf10bef33f39daea666
liubich <liubich.a@gmail.com>
2019-07-13 19:39:54 +0300

    Add sans-serif as second font

--- src/store/store.js
+++ src/store/store.js
@@ -6,9 +6,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     navItems: [
-      { text: 'Features', font: 'PT Sans' },
-      { text: 'Support', font: 'Helvetica Neue' },
-      { text: 'Blog', font: 'Helvetica Neue' },
+      { text: 'Features', font: 'PT Sans, sans-serif' },
+      { text: 'Support', font: 'Helvetica Neue, sans-serif' },
+      { text: 'Blog', font: 'Helvetica Neue, sans-serif' },
     ],
   },
   mutations: {
