eee6174580aa6b1bdf680a4f4ba815af11e731f6
liubich <liubich.a@gmail.com>
2019-07-13 12:14:58 +0300

    Remove useless logo and styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <TinyOne />
   </div>
 </template>
@@ -19,10 +18,5 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
