4553c12fef0e00aa83fd0c43bc1629ed9d9206e8
liubich <liubich.a@gmail.com>
2019-07-13 21:10:32 +0300

    Add texts to component

--- src/components/CarouselPage.vue
+++ src/components/CarouselPage.vue
@@ -1,5 +1,15 @@
 <template>
-
+<header>
+  <h1>
+    {{pageTexts.mainHeader}}
+  </h1>
+  <h2>
+    {{pageTexts.secondHeader}}
+  </h2>
+  <p>
+    {{pageTexts.simpleText}}
+  </p>
+</header>
 </template>
 
 
