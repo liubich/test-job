fa0350d5a0dfc3371470edebb71e68299c41289c
liubich <liubich.a@gmail.com>
2019-07-13 21:15:07 +0300

    Add containers

--- src/components/CarouselPage.vue
+++ src/components/CarouselPage.vue
@@ -1,15 +1,19 @@
 <template>
-<header>
-  <h1>
-    {{pageTexts.mainHeader}}
-  </h1>
-  <h2>
-    {{pageTexts.secondHeader}}
-  </h2>
-  <p>
-    {{pageTexts.simpleText}}
-  </p>
-</header>
+  <header class="carousel-page">
+    <div class="carousel-page__container">
+      <div class="carousel-page__texts-cotainer">
+        <h1>
+          {{pageTexts.mainHeader}}
+        </h1>
+        <h2>
+          {{pageTexts.secondHeader}}
+        </h2>
+        <p>
+          {{pageTexts.simpleText}}
+        </p>
+      </div>
+    </div>
+  </header>
 </template>
 
 
