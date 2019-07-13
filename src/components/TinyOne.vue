9bbbfe1d9e6265300df56e40f0035bf75234670b
liubich <liubich.a@gmail.com>
2019-07-13 13:25:12 +0300

    Add navItems array rendering (navbar)

--- src/components/TinyOne.vue
+++ src/components/TinyOne.vue
@@ -1,11 +1,16 @@
 <template>
-<nav></nav>
+<nav>
+  <ul>
+    <li v-for="(navItem, index) in navItems" :key = "index" :style="{'font-family': navItem.font}">{{navItem.text}}</li>
+  </ul>
+</nav>
 </template>
 
 <script>
 export default {
   name: 'TinyOne',
   props: {
+    navItems: Array,
   },
 };
 </script>
