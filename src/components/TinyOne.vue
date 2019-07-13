5970d3642d5a30d958b7a5f413a8cd5a9c56420f
liubich <liubich.a@gmail.com>
2019-07-13 12:13:58 +0300

    Add empty TinyOne component

---
+++ src/components/TinyOne.vue
@@ -0,0 +1,15 @@
+<template>
+<nav></nav>
+</template>
+
+<script>
+export default {
+  name: 'TinyOne',
+  props: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
