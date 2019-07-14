c830a62201985c160ecdbc89cf08c8ae6114573a
liubich <liubich.a@gmail.com>
2019-07-15 01:45:21 +0300

    Reformat too long lines

--- src/components/FeaturesPage.vue
+++ src/components/FeaturesPage.vue
@@ -4,9 +4,17 @@
       <h1 class="features-page__main-header">{{pageTexts.mainHeader}}</h1>
       <h2 class="features-page__second-header">{{pageTexts.secondHeader}}</h2>
       <div class="features-page__features-container">
-        <div v-for="(feature, index) in pageTexts.features" :key="index" class="features-page__feature feature">
+        <div
+          v-for="(feature, index) in pageTexts.features"
+          :key="index"
+          class="features-page__feature feature"
+        >
           <div class="feature__icon-container">
-            <img class="feature__image" :src="`../../img/${feature.imgName}.png`" :alt="feature.imgName">
+            <img
+              class="feature__image"
+              :src="`../../img/${feature.imgName}.png`"
+              :alt="feature.imgName"
+            >
           </div>
           <dl class="feature__container">
             <dt class="feature__header">{{feature.featureHeader}}</dt>
